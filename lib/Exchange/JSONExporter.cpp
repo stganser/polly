659ac81bf810cdff2c93ba2c43fb12d9b026ad9b
Stefan Ganser <ganser@fim.uni-passau.de>
2016-03-23 13:36:06 +0100

    JSONExporter is able to parse schedule trees and inject them into the current SCoP

--- lib/Exchange/JSONExporter.cpp
+++ lib/Exchange/JSONExporter.cpp
@@ -17,7 +17,9 @@
 #include "polly/ScopInfo.h"
 #include "polly/ScopPass.h"
 #include "polly/Support/ScopLocation.h"
+#include "polly/ScheduleOptimizer.h"
 #include "llvm/ADT/Statistic.h"
+#include "llvm/ADT/DenseSet.h"
 #include "llvm/Analysis/RegionInfo.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/FileSystem.h"
@@ -26,9 +28,13 @@
 #include "llvm/Support/ErrorHandling.h"
 #include "isl/constraint.h"
 #include "isl/map.h"
+#include "isl/union_map.h"
 #include "isl/printer.h"
 #include "isl/set.h"
-#include "isl/union_map.h"
+#include "isl/union_set.h"
+#include "isl/schedule.h"
+#include "isl/schedule_node.h"
+#include "isl/schedule_type.h"
 #include "json/reader.h"
 #include "json/writer.h"
 #include <memory>
@@ -54,6 +60,11 @@ static cl::opt<std::string>
                   cl::desc("Postfix to append to the import .jsop files."),
                   cl::Hidden, cl::value_desc("File postfix"), cl::ValueRequired,
                   cl::init(""), cl::cat(PollyCategory));
+
+static cl::opt<bool> LoadScheduleTree("polly-import-jscop-read-schedule-tree",
+                                      cl::desc("Load the new schedule as a schedule tree."),
+                                      cl::init(false), cl::ZeroOrMore,
+                                      cl::cat(PollyCategory));
 
 struct JSONExporter : public ScopPass {
   static char ID;
@@ -200,6 +211,233 @@ void JSONImporter::printScop(raw_ostream &OS, Scop &S) const {
 
 typedef Dependences::StatementToIslMapTy StatementToIslMapTy;
 
+__isl_give isl_set *getDomainForStmtId(const char *stmtId, Scop &S) {
+  for (ScopStmt &stmt : S) {
+    if (!std::strcmp(stmt.getBaseName(), stmtId)) {
+      return stmt.getDomain();
+    }
+  }
+  return nullptr;
+}
+
+isl_stat foreachSetHelper(isl_set *s, void *user) {
+  std::function<void(isl_set*)> *f = (std::function<void(isl_set*)> *) user;
+  (*f)(s);
+  return isl_stat_ok;
+}
+
+void callLambda(__isl_keep isl_union_set *s, std::function<void(isl_set*)> &f) {
+  isl_union_set_foreach_set(s, foreachSetHelper, &f);
+}
+
+isl_stat foreachMapHelper(isl_map *m, void *user) {
+  std::function<void(isl_map*)> *f = (std::function<void(isl_map*)> *) user;
+  (*f)(m);
+  return isl_stat_ok;
+}
+
+void callLambda(__isl_keep isl_union_map *m, std::function<void(isl_map*)> &f) {
+  isl_union_map_foreach_map(m, foreachMapHelper, &f);
+}
+
+__isl_give isl_schedule_node *scheduleMapHelper(isl_schedule_node *n, void *user) {
+  std::function<isl_schedule_node*(isl_schedule_node*)> *f
+      = (std::function<isl_schedule_node*(isl_schedule_node*)> *) user;
+  return (*f)(n);
+}
+
+__isl_give isl_schedule *callLambda(__isl_take isl_schedule *s,
+                std :: function<isl_schedule_node*(isl_schedule_node*)> &f) {
+  return isl_schedule_map_schedule_node_bottom_up(s, scheduleMapHelper, &f);
+}
+
+DenseSet<const char *> getTupleNamesFromUnionSet(__isl_keep isl_union_set *s) {
+  DenseSet<const char *> names;
+  std::function<void(isl_set *)> lambda = [&names](isl_set *s) {
+    names.insert(isl_set_get_tuple_name(s));
+    isl_set_free(s);
+  };
+  callLambda(s, lambda);
+  return names;
+}
+
+__isl_give isl_union_set *buildDomainForSttmnts(
+    DenseSet<const char*> &stmtNames, Scop &S) {
+  isl_union_set *domain = S.getDomains();
+  isl_union_set *result
+      = isl_union_set_empty(isl_union_set_get_space(domain));
+  isl_union_set_free(domain);
+  for (const char* stmtName : stmtNames) {
+    for (ScopStmt &S : S) {
+      if (!std::strcmp(stmtName, S.getBaseName())) {
+        result = isl_union_set_add_set(result, S.getDomain());
+      }
+    }
+  }
+  return result;
+}
+
+ScopStmt& getStmtForName(const char *name, Scop &S) {
+  for (ScopStmt &Stmt : S) {
+    if (!std::strcmp(name, Stmt.getBaseName())) {
+      return Stmt;
+    }
+  }
+  report_fatal_error("Wat?", true);
+}
+
+__isl_give isl_union_map *replaceIdsInUnionMap(__isl_take isl_union_map *m,
+                                               Scop &S) {
+  isl_union_map *result = nullptr;
+  std::function<void(isl_map *)> lambda = [&result, &S](isl_map *m) {
+    const char* tupleName = isl_map_get_tuple_name(m, isl_dim_in);
+    ScopStmt &sttmt = getStmtForName(tupleName, S);
+    isl_space *stmtSpace = sttmt.getDomainSpace();
+    isl_map *newMap = isl_map_set_tuple_id(m, isl_dim_in,
+                              isl_space_get_tuple_id(stmtSpace, isl_dim_set));
+    for (unsigned i = 0; i < isl_space_dim(stmtSpace, isl_dim_param); ++i) {
+      isl_id *id = isl_space_get_dim_id(stmtSpace, isl_dim_param, i);
+      newMap = isl_map_set_dim_id(newMap, isl_dim_param, i, id);
+    }
+    isl_space_free(stmtSpace);
+
+    if (!result)
+      result = isl_union_map_from_map(newMap);
+    else
+      result = isl_union_map_add_map(result, newMap);
+  };
+  callLambda(m, lambda);
+  isl_union_map_free(m);
+  return result;
+}
+
+__isl_give isl_schedule *setCoincidence(__isl_take isl_schedule *sched,
+                                        __isl_keep isl_schedule_node *oldNode) {
+  isl_schedule_node *root = isl_schedule_get_root(sched);
+  std::function<isl_schedule_node*(isl_schedule_node*)> lambda
+      = [&root, &oldNode](isl_schedule_node *n) {
+    isl_schedule_node *res = n;
+    if (isl_schedule_node_has_parent(n)) {
+      isl_schedule_node *parent = isl_schedule_node_parent(
+            isl_schedule_node_copy(n));
+      if (isl_schedule_node_is_equal(root, parent)) {
+        for (unsigned i = 0; i < isl_schedule_node_band_n_member(res); ++i) {
+          int coincidence = isl_schedule_node_band_member_get_coincident(
+                oldNode, i);
+          res = isl_schedule_node_band_member_set_coincident(res, i,
+                                                             coincidence);
+        }
+      }
+      isl_schedule_node_free(parent);
+    }
+    return res;
+  };
+  isl_schedule *result = callLambda(sched, lambda);
+  isl_schedule_node_free(root);
+  return result;
+}
+
+__isl_give isl_schedule *setPermutability(__isl_take isl_schedule *sched,
+                                      __isl_keep isl_schedule_node *oldNode) {
+  isl_schedule_node *root = isl_schedule_get_root(sched);
+  std::function<isl_schedule_node*(isl_schedule_node*)> lambda
+      = [&root, &oldNode](isl_schedule_node *n) {
+    isl_schedule_node *res = n;
+    if (isl_schedule_node_get_type(n) == isl_schedule_node_band
+        && isl_schedule_node_has_parent(n)) {
+      isl_schedule_node *parent = isl_schedule_node_parent(
+            isl_schedule_node_copy(n));
+      if (isl_schedule_node_is_equal(root, parent)) {
+        res = isl_schedule_node_band_set_permutable(n,
+                                isl_schedule_node_band_get_permutable(oldNode));
+      }
+      isl_schedule_node_free(parent);
+    }
+    return res;
+  };
+  isl_schedule_node_free(root);
+  isl_schedule *result = callLambda(sched, lambda);
+  return result;
+}
+
+__isl_give isl_schedule *rebuildSchedule(__isl_take isl_schedule_node *n,
+                                  Scop &S, __isl_keep isl_union_set *domain) {
+
+  isl_schedule *result = nullptr;
+
+  switch (isl_schedule_node_get_type(n)) {
+    case isl_schedule_node_type::isl_schedule_node_filter : {
+      isl_union_set *filterSet = isl_schedule_node_filter_get_filter(n);
+      DenseSet<const char*> stmtNames = getTupleNamesFromUnionSet(filterSet);
+      isl_union_set *newDomain = buildDomainForSttmnts(stmtNames, S);
+      isl_union_set_free(filterSet);
+
+      result = rebuildSchedule(isl_schedule_node_get_child(n, 0), S,
+                                 newDomain);
+      isl_union_set_free(newDomain);
+      break;
+    }
+
+    case isl_schedule_node_type::isl_schedule_node_domain : {
+      result = rebuildSchedule(isl_schedule_node_get_child(n, 0), S, domain);
+      break;
+    }
+
+    case isl_schedule_node_type::isl_schedule_node_band : {
+      isl_union_map *sched =
+          isl_schedule_node_band_get_partial_schedule_union_map(n);
+      sched = replaceIdsInUnionMap(sched, S);
+      result = rebuildSchedule(isl_schedule_node_get_child(n, 0), S, domain);
+      result = isl_schedule_insert_partial_schedule(result,
+                                isl_multi_union_pw_aff_from_union_map(sched));
+      result = setCoincidence(result, n);
+      result = setPermutability(result, n);
+      break;
+    }
+
+    case isl_schedule_node_type::isl_schedule_node_sequence : {
+      isl_schedule_node *fstChild = isl_schedule_node_get_child(n, 0);
+      result = rebuildSchedule(fstChild, S, domain);
+      for (int i = 1; i < isl_schedule_node_n_children(n); ++i) {
+        isl_schedule *childSched = rebuildSchedule(
+              isl_schedule_node_get_child(n, i), S, domain);
+        result = isl_schedule_sequence(result, childSched);
+      }
+      break;
+    }
+
+    case isl_schedule_node_type::isl_schedule_node_leaf : {
+      result = isl_schedule_from_domain(isl_union_set_copy(domain));
+      break;
+    }
+
+    default : {
+      report_fatal_error(
+          "schedule rebuilding is unimplemented for the current node type.",
+            false);
+    }
+  }
+  isl_schedule_node_free(n);
+  return result;
+}
+
+__isl_give isl_schedule *rebuildSchedule(__isl_take isl_schedule *sched,
+                                         Scop &S) {
+  isl_union_set *domain = S.getDomains();
+  isl_schedule *result = rebuildSchedule(isl_schedule_get_root(sched), S,
+                                         domain);
+  isl_printer *pr = isl_printer_to_str(S.getIslCtx());
+  pr = isl_printer_print_schedule(pr, sched);
+  errs() << "Original schedule: " << isl_printer_get_str(pr) << '\n';
+  pr = isl_printer_flush(pr);
+  pr = isl_printer_print_schedule(pr, result);
+  errs() << "Rebuilt schedule: " << isl_printer_get_str(pr) << '\n';
+  isl_printer_free(pr);
+  isl_schedule_free(sched);
+  isl_union_set_free(domain);
+  return result;
+}
+
 bool JSONImporter::runOnScop(Scop &S) {
   Region &R = S.getRegion();
   const Dependences &D = getAnalysis<DependenceInfo>().getDependences();
@@ -246,24 +484,43 @@ bool JSONImporter::runOnScop(Scop &S) {
 
   StatementToIslMapTy NewSchedule;
 
-  int index = 0;
+  isl_schedule *scheduleTree;
 
-  for (ScopStmt &Stmt : S) {
-    Json::Value schedule = jscop["statements"][index]["schedule"];
-    isl_map *m = isl_map_read_from_str(S.getIslCtx(), schedule.asCString());
-    isl_space *Space = Stmt.getDomainSpace();
+  if (LoadScheduleTree) {
+    errs() << "Loading schedule tree.\n";
+    Json :: Value schedTreeVal = jscop["schedTree"];
+    scheduleTree = isl_schedule_read_from_str(S.getIslCtx(), schedTreeVal.asCString());
+    isl_union_map *schedMap = isl_schedule_get_map(scheduleTree);
+    schedMap = replaceIdsInUnionMap(schedMap, S);
+    std::function<void(isl_map*)> lambda = [&NewSchedule, &S](isl_map *m) {
+      for (ScopStmt &Stmt : S) {
+        if (!strcmp(Stmt.getBaseName(), isl_map_get_tuple_name(m, isl_dim_in))) {
+          NewSchedule[&Stmt] = m;
+        }
+      }
+    };
 
-    // Copy the old tuple id. This is necessary to retain the user pointer,
-    // that stores the reference to the ScopStmt this schedule belongs to.
-    m = isl_map_set_tuple_id(m, isl_dim_in,
-                             isl_space_get_tuple_id(Space, isl_dim_set));
-    for (unsigned i = 0; i < isl_space_dim(Space, isl_dim_param); i++) {
-      isl_id *id = isl_space_get_dim_id(Space, isl_dim_param, i);
-      m = isl_map_set_dim_id(m, isl_dim_param, i, id);
+    callLambda(schedMap, lambda);
+    isl_union_map_free(schedMap);
+  } else {
+    int index = 0;
+    for (ScopStmt &Stmt : S) {
+      Json::Value schedule = jscop["statements"][index]["schedule"];
+      isl_map *m = isl_map_read_from_str(S.getIslCtx(), schedule.asCString());
+      isl_space *Space = Stmt.getDomainSpace();
+
+      // Copy the old tuple id. This is necessary to retain the user pointer,
+      // that stores the reference to the ScopStmt this schedule belongs to.
+      m = isl_map_set_tuple_id(m, isl_dim_in,
+                               isl_space_get_tuple_id(Space, isl_dim_set));
+      for (unsigned i = 0; i < isl_space_dim(Space, isl_dim_param); i++) {
+        isl_id *id = isl_space_get_dim_id(Space, isl_dim_param, i);
+        m = isl_map_set_dim_id(m, isl_dim_param, i, id);
+      }
+      isl_space_free(Space);
+      NewSchedule[&Stmt] = m;
+      index++;
     }
-    isl_space_free(Space);
-    NewSchedule[&Stmt] = m;
-    index++;
   }
 
   if (!D.isValidSchedule(S, &NewSchedule)) {
@@ -278,15 +535,24 @@ bool JSONImporter::runOnScop(Scop &S) {
     report_fatal_error(errMsg, false);
   }
 
-  auto ScheduleMap = isl_union_map_empty(S.getParamSpace());
-  for (ScopStmt &Stmt : S) {
-    if (NewSchedule.find(&Stmt) != NewSchedule.end())
-      ScheduleMap = isl_union_map_add_map(ScheduleMap, NewSchedule[&Stmt]);
-    else
-      ScheduleMap = isl_union_map_add_map(ScheduleMap, Stmt.getSchedule());
+  if (LoadScheduleTree) {
+    scheduleTree = rebuildSchedule(scheduleTree, S);
+    scheduleTree = ScheduleTreeOptimizer::optimizeSchedule(scheduleTree);
+    S.setScheduleTree(scheduleTree);
+    S.markAsOptimized();
+    for (ScopStmt &Stmt : S) {
+      isl_map_free(NewSchedule[&Stmt]);
+    }
+  } else {
+    auto ScheduleMap = isl_union_map_empty(S.getParamSpace());
+    for (ScopStmt &Stmt : S) {
+      if (NewSchedule.find(&Stmt) != NewSchedule.end())
+        ScheduleMap = isl_union_map_add_map(ScheduleMap, NewSchedule[&Stmt]);
+      else
+        ScheduleMap = isl_union_map_add_map(ScheduleMap, Stmt.getSchedule());
+    }
+    S.setSchedule(ScheduleMap);
   }
-
-  S.setSchedule(ScheduleMap);
 
   int statementIdx = 0;
   for (ScopStmt &Stmt : S) {
